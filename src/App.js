3879451d4425e6bcb68e45f7191ab0b58687cee1
Matthew Howard <mattalanhoward@Matthews-MBP.askey.com>
2022-12-09 22:54:45 -0500

    Refactor Routes, Create 404 page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import Landing from "./pages/Landing";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dashboard, Error, Landing, Register } from "./pages";
 
 function App() {
 	return (
 		<BrowserRouter>
-			<nav>
-				<Link to="/">Dashboard</Link>
-				<Link to="/register">Register</Link>
-				<Link to="/landing">Home</Link>
-			</nav>
 			<Routes>
-				<Route path="/" element={<div>Dashboard</div>}></Route>
-				<Route path="register" element={<div>Register</div>}></Route>
+				<Route path="/" element={<Dashboard />}></Route>
+				<Route path="register" element={<Register />}></Route>
 				<Route path="/landing" element={<Landing />}></Route>
-				<Route path="*" element={<div>404</div>}></Route>
+				<Route path="*" element={<Error />}></Route>
 			</Routes>
 		</BrowserRouter>
 	);
