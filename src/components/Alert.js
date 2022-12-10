11a39da6755609ab775da3620162a463cc5a3d01
Matthew Howard <mattalanhoward@Matthews-MBP.askey.com>
2022-12-10 00:24:02 -0500

    Create Register Page

---
+++ src/components/Alert.js
@@ -0,0 +1,4 @@
+const Alert = () => {
+	return <div className="alert alert-danger">Alert Goes Here</div>;
+};
+export default Alert;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,72 @@
-const Register = () => {
-	return <div>Register</div>;
+import { useState, useEffect } from "react";
+import { Logo, FormRow, Alert } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialsState = {
+	name: "",
+	email: "",
+	password: "",
+	isMember: "",
+	showAlert: false,
 };
+const Register = () => {
+	const [values, setValues] = useState(initialsState);
+	// global state and useNavigate
+
+	const toggleMember = () => {
+		setValues({ ...values, isMember: !values.isMember });
+	};
+
+	const handleChange = (e) => {
+		console.log(`handleChange`, e.target);
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		console.log(`onSubmit`, e.target);
+	};
+
+	return (
+		<Wrapper className="full-page">
+			<form className="form" onSubmit={onSubmit}>
+				<Logo />
+				<h3>{values.isMember ? "Login" : "Register"}</h3>
+				{values.showAlert && <Alert />}
+				{/*name input*/}
+				{!values.isMember && (
+					<FormRow
+						type="text"
+						name="name"
+						value={values.name}
+						handleChange={handleChange}
+					/>
+				)}
+				{/*email input*/}
+				<FormRow
+					type="email"
+					name="email"
+					value={values.email}
+					handleChange={handleChange}
+				/>
+				{/*password input*/}
+				<FormRow
+					type="password"
+					name="password"
+					value={values.password}
+					handleChange={handleChange}
+				/>
+				<button type="submit" onSubmit={onSubmit} className="btn btn-block">
+					Submit
+				</button>
+				<p>
+					{values.isMember ? "Not a member yet?" : "Already a member?"}
+					<button type="button" onClick={toggleMember} className="member-btn">
+						{values.isMember ? "Register" : "Login"}
+					</button>
+				</p>
+			</form>
+		</Wrapper>
+	);
+};
+
 export default Register;
