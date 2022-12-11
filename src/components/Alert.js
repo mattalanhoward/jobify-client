c25ae1daadc8d73863b26b79e193fb0c576f4187
Matthew Howard <mattalanhoward@Matthews-MBP.askey.com>
2022-12-10 23:53:23 -0500

    Setup global state and toggle showAlert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,4 +1,8 @@
+import { useAppContext } from "../context/appContext";
+
 const Alert = () => {
-	return <div className="alert alert-danger">Alert Goes Here</div>;
+	const { alertType, alertText } = useAppContext();
+
+	return <div className={`alert alert-${alertType}`}>{alertText}</div>;
 };
 export default Alert;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,29 +1,39 @@
 import { useState, useEffect } from "react";
 import { Logo, FormRow, Alert } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
+import { useAppContext } from "../context/appContext";
 
 const initialsState = {
 	name: "",
 	email: "",
 	password: "",
 	isMember: "",
-	showAlert: false,
 };
+
 const Register = () => {
 	const [values, setValues] = useState(initialsState);
 	// global state and useNavigate
+	const { isLoading, showAlert, displayAlert, clearAlert } = useAppContext();
 
 	const toggleMember = () => {
 		setValues({ ...values, isMember: !values.isMember });
 	};
 
 	const handleChange = (e) => {
-		console.log(`handleChange`, e.target);
+		setValues({ ...values, [e.target.name]: e.target.value });
 	};
 
 	const onSubmit = (e) => {
 		e.preventDefault();
-		console.log(`onSubmit`, e.target);
+		const { name, email, password, isMember } = values;
+
+		if (!email || !password || (!isMember && !name)) {
+			displayAlert();
+			return;
+		} else {
+			clearAlert();
+			console.log(values);
+		}
 	};
 
 	return (
@@ -31,7 +41,7 @@ const Register = () => {
 			<form className="form" onSubmit={onSubmit}>
 				<Logo />
 				<h3>{values.isMember ? "Login" : "Register"}</h3>
-				{values.showAlert && <Alert />}
+				{showAlert && <Alert />}
 				{/*name input*/}
 				{!values.isMember && (
 					<FormRow
