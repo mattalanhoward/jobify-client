46160f41e77c50eb52bf764e13e65e3ad47de3cb
Matthew Howard <mattalanhoward@gmail.com>
2023-01-11 11:48:38 -0500

    Pagination

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -31,6 +31,7 @@ import {
 	SHOW_STATS_BEGIN,
 	SHOW_STATS_SUCCESS,
 	CLEAR_FILTERS,
+	CHANGE_PAGE,
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -238,8 +239,8 @@ const AppProvider = ({ children }) => {
 	};
 
 	const getJobs = async () => {
-		const { search, searchStatus, searchType, sort } = state;
-		let url = `/jobs?status=${searchStatus}&jobType=${searchType}&sort=${sort}`;
+		const { page, search, searchStatus, searchType, sort } = state;
+		let url = `/jobs?page=${page}&status=${searchStatus}&jobType=${searchType}&sort=${sort}`;
 		if (search) {
 			url = url + `&search=${search}`;
 		}
@@ -321,6 +322,10 @@ const AppProvider = ({ children }) => {
 		dispatch({ type: CLEAR_FILTERS });
 	};
 
+	const changePage = (page) => {
+		dispatch({ type: CHANGE_PAGE, payload: { page } });
+	};
+
 	useEffect(() => {
 		getJobs();
 	}, []);
@@ -345,6 +350,7 @@ const AppProvider = ({ children }) => {
 				editJob,
 				showStats,
 				clearFilters,
+				changePage,
 			}}
 		>
 			{children}
